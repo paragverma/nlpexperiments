91a1a6c1c70337173c6911d3525c84ccedf61df4
paragverma <vermap14.comp@coep.ac.in>
2017-10-25 00:56:42 +0530

    Create hpost.py

---
+++ rstapicall/hpost.py
@@ -0,0 +1,39 @@
+import requests
+import sys
+import re
+
+url = "http://alt.qcri.org/demos/Discourse_Parser_Demo/"
+
+def getRST(sent):
+
+	payload = {}
+	payload["textdata"] = sent
+	payload["discourse"] = "b"
+	payload["submitsave"] = "Execute"
+
+	#print(payload)
+	r = requests.post(url, data=payload)
+
+	res = str(r.text.encode(sys.stdout.encoding, errors='replace'))
+
+	#fl = open("op.txt", "w")
+
+	#fl.write(res)
+	#imp_parts = re.findall(r"\*[^\*]*\*", u_inp)
+
+	ind = res.index("<script type=\"text/javascript\">rhetoricParsingOutput=\"")
+
+	ind = ind + len("<script type=\"text/javascript\">rhetoricParsingOutput=\"")
+	#print(ind)
+
+	op = ""
+
+	ind += 1
+	while True:
+		if res[ind] == "\"":
+			break
+		
+		op += res[ind]
+		ind += 1
+		
+	return(op)
