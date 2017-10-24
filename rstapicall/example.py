ea82b77549826d06ec0c70f61e6104470acd40d2
paragverma <vermap14.comp@coep.ac.in>
2017-10-25 01:51:15 +0530

    Update example.py

--- rstapicall/example.py
+++ rstapicall/example.py
@@ -2,4 +2,9 @@ import hpost
 
 mysent = "There are some people who think that only the poor and less educated people use slang, but this idea is erroneous."
 
-print(hpost.getRST(mysent))
+res = hpost.getRST(mysent)
+print(res)
+
+phrase = hpost.getphrase(mysent, res, 20)
+
+print(phrase)
